ddd52aa3dded0581f719a951e98116e86cc81aea
AllenKayy <allisonoyinkansola1@gmail.com>
2023-10-18 17:05:43 +0100

    Properly handle the carousel dynamic rendering

--- script.js
+++ script.js
@@ -50,103 +50,133 @@ const displayDropDown = () => {
 displayDropDown();
 window.addEventListener('resize', displayDropDown)
 
+let newsData;
+
 // Function to display news data 
 const displayTopHeadlines = (news) => {
-  // Top Headlines
   const sliderEl = document.querySelector(".slider");
   const carouselOverview = document.querySelector(".carousel_overview");
+  const size = 4;
+  let currentIndex = 0; // Initialize the current index
 
-  sliderEl.innerHTML =`
-    <div class="carousel_card id="each_news">
-      <img src="${news.urlToImage}" alt="" class="cover_img" draggable="false">
-    </div>
-    `
-  
-  carouselOverview.innerHTML = `
-    <div class="top_overview">
-      <div>
-          <span>${news.publishedAt} &#8226;</span>
-          <span>By ${news.author}</span>
-      </div>
-      <div>TECHNOLOGY</div>
-    </div>
-    <h1>${news.title}</h1>
-    <p>${news.description}</p>
-    <div class="carousel_navigation flex">
-      <ul class="navigation flex">
-          <li class="selected"></li>
-          <li></li>
-          <li></li>
-          <li></li>
-      </ul>
-      <div class="carousel_btn flex">
-        <div class="arrow_btn prev flex">
-          <img src="./assets/svg/arrow-right-solid.svg" alt="" width="14">
+  // Function to update the carousel overview based on the current index
+  const updateOverview = (index) => {
+    const currentNews = news[index];
+    carouselOverview.innerHTML = `
+      <div class="top_overview">
+        <div>
+            <span>${currentNews.publishedAt} &#8226;</span>
+            <span>By ${currentNews.author}</span>
         </div>
-        <div class="arrow_btn next flex">
-          <img src="./assets/svg/arrow-right-solid.svg" alt="" width="14">
+        <div>TECHNOLOGY</div>
+      </div>
+      <h1>${currentNews.title}</h1>
+      <p>${currentNews.description}</p>
+      <div class="carousel_navigation flex">
+        <ul class="navigation flex">
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+        </ul>
+        <div class="carousel_btn flex">
+          <div class="arrow_btn prev flex">
+            <img src="./assets/svg/arrow-right-solid.svg" alt="" width="14">
+          </div>
+          <div class="arrow_btn next flex">
+            <img src="./assets/svg/arrow-right-solid.svg" alt="" width="14">
+          </div>
         </div>
       </div>
-    </div>
-  ` 
-}
+    `;
+  };
+
+  // Initialize the carousel overview
+  updateOverview(currentIndex);
+
+  // Event listener for scrolling or other interactions that change the current index
+  sliderEl.addEventListener('scroll', () => {
+    // Calculate the current index based on slider position
+    const cardWidth = document.querySelector(".carousel_card").offsetWidth;
+    currentIndex = Math.round(sliderEl.scrollLeft / cardWidth);
+
+    // Update the carousel overview based on the current index
+    updateOverview(currentIndex);
+  });
+
+  // Rest of your code for displaying carousel cards
+  sliderEl.innerHTML = '';
+  news.slice(0, size).forEach(news => {
+    sliderEl.innerHTML += `
+      <div class="carousel_card">
+        <img src="${news.urlToImage}" alt="" class="cover_img" draggable="false">
+      </div>
+    `;
+  });
+};
+
 
 // Function to display side news
-const displaySideNews = (props) => {
+const displaySideNews = (news) => {
   const sideNews = document.querySelector(".right_news");
+  sideNews.innerHTML = ''; 
+  const size = 6; 
 
-  sideNews.innerHTML = `
-    <div class="right_news_item border_bottom" id="each_news">
-      <img src="${news.urlToImage}" alt="">
-      <h3 class="bg_text">${news.title}</h3>
-      <p class="md_text">${news.description}</p>
-      <span>${news.publishedAt} &#8226;</span>
-      <span>By ${news.author}</span>
-    </div>
-  `
+  news.slice(0, size).forEach((newsItem, index) => {
+    const newsCard = document.createElement('div');
+    newsCard.className = "right_news_item border_bottom";
+    // newsCard.id = "each_news";
+
+    newsCard.innerHTML = `
+      <img src="${newsItem.urlToImage}" alt="">
+      <h3 class="bg_text">${newsItem.title}</h3>
+      <p class="md_text">${newsItem.description}</p>
+      <span>${newsItem.publishedAt} &#8226;</span>
+      <span>By ${newsItem.author}</span>
+    `;
+
+    sideNews.appendChild(newsCard);
+  });
 }
+
 // Function to display side news
-const displayBottomNews = (props) => {
+const displayBottomNews = (news) => {
   const bottomNews = document.querySelector(".bottom_news");
+  const size = 2;
 
-  bottomNews.innerHTML = `
-    <div class="bottom_news_content" id="each_news">
-      <h3 class="bg_text">${news.title}</</h3>
-      <p class="md_text">${news.description}</</p>
-    </div>
-  `
+  news.slice(0, size).forEach(news => {
+    bottomNews.innerHTML += `
+      <div class="bottom_news_content" id="each_news">
+        <h3 class="bg_text">${news.title}</h3>
+        <p class="md_text">${news.description}</p>
+      </div>
+    `
+  })
+  
 }
 
-// const APIURL = "https://newsapi.org/v2/everything?q=-sex"
+const APIURL = "https://newsapi.org/v2/everything?q=-sex"
 // 099148be22804e849a0c6fe022b7cf5e
 fetch(APIURL, {
   method: 'GET',
   headers: {
-      // 'X-Api-Key': '198d6676e82741a5b83c7db74ae51e0a'
+      'X-Api-Key': '198d6676e82741a5b83c7db74ae51e0a'
   }
 })
 .then(res => res.json())
 .then(data => {
-  const articles = data.articles
+  const { articles } = data
   displayNews(articles);
-  // articles.forEach(props => {
-  //   // console.log(props.content);
-  //   displayNwes(props);
-  //   displayTopHeadlines(props);
-  //   displayBottomNews(props);
-  //   displaySideNews(props);
-  // });
+
 })
 .catch(err => console.error("Error fetching data:", err))
 
-function displayNews(newsData) {
-  newsData.forEach((news) => {
+function displayNews(news) {
     displayTopHeadlines(news);
     displayBottomNews(news);
     displaySideNews(news);
-  });
 
-  const newsElements = document.querySelectorAll('each_news');
+  const newsElements = document.querySelectorAll('#each_news');
   newsElements.forEach((element, index) => {
     element.addEventListener('click', () => {
       displayDetailsPage(newsData[index]);
@@ -176,21 +206,24 @@ searchInput.addEventListener('input', () => {
 
 
 
-// const sliderEl = document.querySelector(".slider");
+const sliderEl = document.querySelector(".slider");
 const carouselBtn = document.querySelectorAll(".carousel_btn div");
 const navigationItems = document.querySelectorAll(".navigation li");
 const carouselCards = document.querySelectorAll(".carousel_card");
-const cardWidth = carouselCards[0].offsetWidth;
 let currentIndex = 0;
 
 // Function to update the carousel to the given index
 function updateCarousel(index) {
-  sliderEl.scrollTo({
-    left: index * cardWidth,
-    behavior: "smooth",
+  const carouselCards = document.querySelectorAll(".carousel_card");
+  const cardWidth = carouselCards[0].offsetWidth;
+  requestAnimationFrame(() => {
+    sliderEl.scrollTo({
+      left: index * cardWidth,
+      behavior: "smooth",
+    });
+    currentIndex = index;
+    updateNavigation(index);
   });
-  currentIndex = index;
-  updateNavigation(index);
 }
 
 // Function to update the navigation indicators
@@ -233,15 +266,15 @@ const infiniteScroll = (index) => {
   return index;
 }
 
-// // Function to scroll to the next card
-// function scrollToNextCard() {
-//   updateCarousel(infiniteScroll(currentIndex + 1));
-// }
+// Function to scroll to the next card
+function scrollToNextCard() {
+  updateCarousel(infiniteScroll(currentIndex + 1));
+}
 
-// // Function to scroll to the previous card
-// function scrollToPrevCard() {
-//   updateCarousel(infiniteScroll(currentIndex - 1));
-// }
+// Function to scroll to the previous card
+function scrollToPrevCard() {
+  updateCarousel(infiniteScroll(currentIndex - 1));
+}
 
 // Event listeners for navigation buttons
 carouselBtn.forEach((btn) => {
@@ -295,10 +328,12 @@ const scroll = (e) => {
 const stopScroll = () => {
   isDragging = false;
   sliderEl.style.scrollBehavior = "smooth";
-  const newIndex = infiniteScroll(
-    Math.round(sliderEl.scrollLeft / cardWidth)
-  );
-  updateCarousel(newIndex);
+  const carouselCards = document.querySelectorAll(".carousel_card");
+    const cardWidth = carouselCards[0].offsetWidth;
+    const newIndex = infiniteScroll(
+      Math.round(sliderEl.scrollLeft / cardWidth)
+    );
+    updateCarousel(newIndex);
 };
 
 const scrollDefault = () => {
@@ -306,17 +341,17 @@ const scrollDefault = () => {
   sliderEl.style.scrollBehavior = "smooth";
 };
 
-// // Additional logic to handle infinite scrolling by hand
-// const wrapScroll = () => {
-//   const maxScrollLeft = cardWidth * (carouselCards.length - 1);
-//   if (sliderEl.scrollLeft === 0) {
-//     // User scrolled to the beginning, wrap to the end
-//     sliderEl.scrollLeft = maxScrollLeft;
-//   } else if (sliderEl.scrollLeft === maxScrollLeft + cardWidth) {
-//     // User scrolled to the end, wrap to the beginning
-//     sliderEl.scrollLeft = cardWidth;
-//   }
-// };
+// Logic to handle infinite scrolling by hand
+const wrapScroll = () => {
+  const maxScrollLeft = cardWidth * (carouselCards.length - 1);
+  if (sliderEl.scrollLeft === 0) {
+    // User scrolled to the beginning, wrap to the end
+    sliderEl.scrollLeft = maxScrollLeft;
+  } else if (sliderEl.scrollLeft === maxScrollLeft + cardWidth) {
+    // User scrolled to the end, wrap to the beginning
+    sliderEl.scrollLeft = cardWidth;
+  }
+};
 
 sliderEl.addEventListener("mousemove", scroll);
 sliderEl.addEventListener("mousedown", startScroll);
